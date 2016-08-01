a695727e179e1d150bb8b998c8942f869774290f
Peter Neave <p_neave@yahoo.com>
2016-08-01 21:49:14 +1000

    Update to Builder Unit test

--- Builder/BuilderUnitTest/BuilderUnitTest.cs
+++ Builder/BuilderUnitTest/BuilderUnitTest.cs
@@ -9,31 +9,43 @@ namespace BuilderUnitTest
     [TestClass]
     public class BuilderUnitTest
     {
-        /*
-        public House BuildHouse<T>()
+
+        public House BuildHouse<T>() where T : HouseBuilder, new()
         {
             HouseBuilder houseBuilder = new T();
             var siteManager = new SiteManager(houseBuilder);
-
             siteManager.BuildHouse();
             return siteManager.GetHouse();
         }
-        */
-
 
         [TestMethod]
-        public void Builder_GivenTownHouseBuilder_BuildsBeachHouse()
+        public void Builder_GivenBeachHouseBuilder_BuildsBeachHouse()
         {
-            var houseBuilder = new BeachHouseBuilder();
-            var siteManager = new SiteManager(houseBuilder);
-
-            siteManager.BuildHouse();
-            var house = siteManager.GetHouse();
+            var house = BuildHouse<BeachHouseBuilder>();
 
             IsNotNull(house);
             AreEqual("Beach", house.Type);
             Console.Write(house);
         }
 
+        [TestMethod]
+        public void Builder_GivenTownHouseBuilder_BuildsTownHouse()
+        {
+            var house = BuildHouse<TownHouseBuilder>();
+
+            IsNotNull(house);
+            AreEqual("Town", house.Type);
+            Console.Write(house);
+        }
+
+        [TestMethod]
+        public void Builder_GivenMansionBuilder_BuildsMansionHouse()
+        {
+            var house = BuildHouse<MansionHouseBuilder>();
+
+            IsNotNull(house);
+            AreEqual("Mansion", house.Type);
+            Console.Write(house);
+        }
     }
 }
